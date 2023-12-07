2017235afb588ee53896ffaf403f5fdedf7133f9
Peavy101 <matthewhardy709@gmail.com>
2023-12-07 16:08:50 -0330

    Working on improving layout

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const code = document.getElementById('code').value;
+    const capitalLetters = getCapitalLetters(code);
 
-
-    displayCode.innerHTML = code;
+    displayCode.innerHTML = capitalLetters;
 })
+
+function getCapitalLetters(text) {
+    let result = '';
+    for (let i = 0; i < text.length; i++) {
+        const currentChar = text.charAt(i);
+        if (currentChar >= 'A' && currentChar <= 'Z') {
+            result += currentChar;
+        }
+    }
+    return result;
+}
+
+
+//sample phrases: Here Is a Mystery On a Message
